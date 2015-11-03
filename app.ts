9d77b57b0193ed3c487c5e3586bf0e87d4f81059
alexmunoz905 <alex.munoz905@gmail.com>
2015-11-03 08:25:21 -0500

    Html and ts

---
+++ app.ts
@@ -0,0 +1,7 @@
+    import {Component, bootstrap} from 'angular2/angular2';
+    @Component({
+        selector: 'my-app',
+        template: '<h1>Testing....</h1>'
+    })
+    class AppComponent { }
+    bootstrap(AppComponent);
